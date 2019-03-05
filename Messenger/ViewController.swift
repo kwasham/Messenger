149838af9e78ffd7e3e728e75a39b479ea72c69c
kwasham <kawasham@att.net>
2019-03-05 17:10:50 -0600

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Kirk Washam on 3/5/19.
+//  Copyright © 2019 StudioATX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
